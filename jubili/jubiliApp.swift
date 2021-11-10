59e270047f5a789271fe7c12877f8ca53a654179
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-11-10 23:04:02 +0500

    Initial Commit

---
+++ jubili/jubiliApp.swift
@@ -0,0 +1,17 @@
+//
+//  jubiliApp.swift
+//  jubili
+//
+//  Created by Muhammad Khan on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct jubiliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

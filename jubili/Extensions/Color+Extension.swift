89e7d32f02dda423ec789af59795078d1437fb99
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-11-12 18:13:53 +0500

    added some extensions with extra functionality

---
+++ jubili/Extensions/Color+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Extension.swift
+//  jubili
+//
+//  Created by Muhammad Khan on 11/12/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let appBlue = Color(red: 24/255, green: 62/255, blue: 245/255)
+    static let appLightGrayBG = Color(red: 243/255, green: 242/255, blue: 242/255)
+    static let appLightGrayLine = Color(red: 218/255, green: 217/255, blue: 217/255)
+    static let appLighterBlue = Color(red: 236/255, green: 240/255, blue: 254/255)
+    static let appLightGrayText = Color(red: 158/255, green: 157/255, blue: 157/255)
+    static let appGreenText = Color(red: 81/255, green: 178/255, blue: 131/255)
+    static let appRedText = Color(red: 179/255, green: 83/255, blue: 80/255)
+}
---
+++ jubili/Extensions/UIScreen+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIScreen+Extension.swift
+//  jubili
+//
+//  Created by Muhammad Khan on 11/12/21.
+//
+
+import SwiftUI
+
+extension UIScreen {
+    static let screenWidth = UIScreen.main.bounds.size.width
+    static let screenHeight = UIScreen.main.bounds.size.height
+    static let screenSize = UIScreen.main.bounds.size
+}

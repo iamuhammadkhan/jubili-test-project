0f829cf1fe02dc66c7b5e853456556e6b76d171c
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-11-15 20:05:58 +0500

    Cart view UI completed

--- jubili/Modules/CartView/CartView.swift
+++ jubili/Modules/CartView/CartView.swift
@@ -20,6 +20,61 @@ struct CartView: View {
                 VStack(alignment: .leading, spacing: 20) {
                     Text("My Cart")
                         .font(.system(size: 22, weight: .semibold, design: .default))
+                    HStack(alignment: .center, spacing: 20) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 8)
+                                .foregroundColor(colorScheme == .dark ? .appDarkGrayBG : .appLightGrayBG)
+                                .frame(width: 125, height: 125, alignment: .center)
+                            Image("5")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 120, height: 120, alignment: .center)
+                        }
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("AKG N700NCM2 Wireless Headphone")
+                                .font(.system(size: 18, weight: .semibold, design: .default))
+                            Text("$199.00 (~$4.00 Tax)")
+                                .foregroundColor(.appLightGrayText)
+                                .font(.system(size: 14, weight: .medium, design: .default))
+                            HStack(alignment: .center, spacing: 0) {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 12)
+                                        .frame(width: 30, height: 30, alignment: .center)
+                                        .foregroundColor(.clear)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 12)
+                                                .stroke(Color.appLightGrayBG, lineWidth: 2)
+                                        )
+                                    Text("-")
+                                        .foregroundColor(.appLightGrayText)
+                                        .font(.system(size: 20, weight: .semibold, design: .default))
+                                }
+                                
+                                Text("1")
+                                    .foregroundColor(.appLightGrayText)
+                                    .font(.system(size: 14, weight: .medium, design: .default))
+                                    .frame(width: 40, height: 30, alignment: .center)
+                                
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 12)
+                                        .frame(width: 30, height: 30, alignment: .center)
+                                        .foregroundColor(.clear)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 12)
+                                                .stroke(Color.appLightGrayBG, lineWidth: 2)
+                                        )
+                                    Text("+")
+                                        .foregroundColor(.appLightGrayText)
+                                        .font(.system(size: 20, weight: .semibold, design: .default))
+                                }
+                                
+                                Spacer()
+                                RoundedRectangle(cornerRadius: 12)
+                                    .foregroundColor(colorScheme == . dark ? .appDarkGrayBG : .appLightGrayBG)
+                                    .frame(width: 30, height: 30, alignment: .center)
+                            }.padding(.top)
+                        }
+                    }
                 }.padding()
                 
                 VStack(alignment: .leading, spacing: 20) {
@@ -32,7 +87,30 @@ struct CartView: View {
                     Text("Payment Method")
                         .font(.system(size: 22, weight: .semibold, design: .default))
                     
-                    
+                    HStack(alignment: .center, spacing: 20) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 6)
+                                .foregroundColor(colorScheme == .dark ? .appDarkGrayBG : .appLightGrayBG)
+                                .frame(width: 50, height: 30, alignment: .center)
+                            Image("visa")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 30, alignment: .center)
+                        }
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("VISA Classic")
+                                .font(.system(size: 18, weight: .semibold, design: .default))
+                            Text("****-0921")
+                                .foregroundColor(.appLightGrayText)
+                                .font(.system(size: 14, weight: .medium, design: .default))
+                        }
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.appDarkBG)
+                            .frame(width: 20, height: 20, alignment: .center)
+                    }
                 }.padding()
                 
                 VStack(alignment: .leading, spacing: 8) {
